8ddb08e9a870f1c006c40cd9a14a07f622cb06d2
unknown <779963847@qq.com>
2021-03-21 16:38:54 +0800

    添加个人中心部分逻辑

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,3 +1,26 @@
-Page({
+const app = getApp();
 
+Page({
+    data: {
+        userInfo: {},
+        hasUserInfo: false
+    },
+    onLoad() {
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            });
+        } else {
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            });
+        }
+    }
 });
